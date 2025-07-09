24becb5071dff842edc3ff96b9d36a5bafa3817b
JoshuaPeters <jgp2.11.99@gmail.com>
2025-07-09 09:08:57 -0600

    added tokens and enhanced lexer

--- token/token.go
+++ token/token.go
@@ -18,6 +18,16 @@ const (
 	// Operators
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ = "=="
+	NOT_EQ = "!="
 
 	// Delimiters
 	COMMA = ","
@@ -31,6 +41,11 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType {
